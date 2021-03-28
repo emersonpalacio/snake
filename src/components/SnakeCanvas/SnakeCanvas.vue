3b0a02e27c644edbd4e700fcc5bf50ec2dba8289
emersonpalacio <53702398+emersonpalacio@users.noreply.github.com>
2021-03-28 16:46:21 -0500

    Initial commit

---
+++ src/components/SnakeCanvas/SnakeCanvas.vue
@@ -0,0 +1,12 @@
+<template>
+  <canvas
+    ref="board"
+    id="snake-canvas"
+    :width="boardSizePx"
+    :height="boardSizePx"
+  ></canvas>
+</template>
+
+<script src="./SnakeCanvas.js"></script>
+
+<style scoped src="./SnakeCanvas.css"></style> 
